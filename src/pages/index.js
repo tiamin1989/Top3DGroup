41d52e8f7d32a3ae1d9f9997d2402e7dfb21a031
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-02-16 16:57:05 +0500

    add: header__contacts element

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+/* const dropdowns = document.querySelectorAll('li.header__dropdown-content');
+const dropdownClick = (evt) => {
+  if (evt.target.classList.contains('header__dropdown-content'))
+  evt.target.querySelector('ul.header__list').classList.remove('header__list_hidden');
+};
+
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener('click', dropdownClick);
+});
+ */
