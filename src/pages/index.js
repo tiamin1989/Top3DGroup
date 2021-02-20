c8658f1251f692ed8868d4929522b6ef116d2a84
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-02-20 18:36:15 +0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,7 @@
 import './index.css';
 
-/* const dropdowns = document.querySelectorAll('li.header__dropdown-content');
-const dropdownClick = (evt) => {
-  if (evt.target.classList.contains('header__dropdown-content'))
-  evt.target.querySelector('ul.header__list').classList.remove('header__list_hidden');
-};
+window.onload = () => {
 
-dropdowns.forEach((dropdown) => {
-  dropdown.addEventListener('click', dropdownClick);
-});
- */
+  /* код для слайдера */
+
+}
